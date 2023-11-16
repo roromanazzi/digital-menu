6a08f69cd9cbb944f2fc3d5fb579a16c098406fd
Rocio <rocioromanazzi@gmail.com>
2023-11-16 16:58:19 -0300

    Fixed styling

--- src/components/Products/Sections.jsx
+++ src/components/Products/Sections.jsx
@@ -1,5 +1,6 @@
 import Product from "./Product";
 import "./Sections.css";
+import doradoImage from "../../img/dorado.png";
 
 const Sections = ({ sections }) => {
   let sectionClassName = "section_container";
@@ -17,7 +18,7 @@ const Sections = ({ sections }) => {
             }
           >
             {section.type === "common" && (
-              <img src="/src/img/dorado.png" className="side_img" />
+              <img src={doradoImage} className="side_img" />
             )}
             <div className="section_info">
               <h3 className="section_title">{section.title}</h3>
